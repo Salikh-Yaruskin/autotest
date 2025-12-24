4e2f2e6dcc2eed9e3118b0cef2b222a4e34e1206
salih <salih1504200473@mail.ru>
2025-12-25 02:25:53 +0400

    Задача U7. Параллельный запуск тестов

---
+++ scripts/grid-node.bat
@@ -0,0 +1,6 @@
+@echo off
+setlocal
+
+set "SELENIUM_SERVER_JAR=tools\selenium\selenium-server-4.39.0.jar"
+set "HUB_URL=http://localhost:4444"
+java -jar "%SELENIUM_SERVER_JAR%" node --hub "%HUB_URL%" --max-sessions 3
